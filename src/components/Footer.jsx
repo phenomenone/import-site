70c9819d0df662f63979f257adfb1dcc3a8ce779
Eden Castillo <ecastilloasport@gmail.com>
2022-05-24 15:50:06 -0500

    [Add] Add missing hrefs for footer social.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
 function Footer(){
     return (
       <div className="social-container">
-        <a href="#"><FontAwesomeIcon icon={faTwitter}/><span>twitter</span></a>
-        <a href="#"><FontAwesomeIcon icon={faFacebook}/><span>facebook</span></a>
-        <a href="#"><FontAwesomeIcon icon={faInstagram}/><span>Instagram</span></a>
-        <a href="#"><FontAwesomeIcon icon={faEnvelope}/><span>Email</span></a>
+        <a href="https://www.twitter.com/" target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faTwitter}/><span>twitter</span></a>
+        <a href="https://www.facebook.com/" target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faFacebook}/><span>facebook</span></a>
+        <a href="https://www.instagram.com/" target="_blank" rel='noreferrer'><FontAwesomeIcon icon={faInstagram}/><span>Instagram</span></a>
+        <a href="mailto:ventas@desdeelmundo.com"><FontAwesomeIcon icon={faEnvelope}/><span>Email</span></a>
 
     </div>
     );
