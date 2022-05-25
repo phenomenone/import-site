99258593daa67aac32bdf7b0fe60cacf0f65d830
Eden Castillo <ecastilloasport@gmail.com>
2022-05-25 12:53:40 -0500

    [Imp] Improved ovver all styling, design and responsiveness for the different media sizes. Update footer with different classes. Remove span animation.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,25 +13,21 @@ function Footer() {
       <div className="social-item">
         <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faTwitter} />
-          <span>twitter</span>
         </a>
       </div>
       <div className="social-item">
         <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faFacebook} />
-          <span>facebook</span>
         </a>
       </div>
       <div className="social-item">
         <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faInstagram} />
-          <span>Instagram</span>
         </a>
       </div>
       <div className="social-item">
         <a href="mailto:ventas@desdeelmundo.com">
           <FontAwesomeIcon icon={faEnvelope} />
-          <span>Email</span>
         </a>
       </div>
     </div>
