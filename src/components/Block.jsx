902f53b2c005c39f5ea09607ab81705952f36201
Eden Castillo <ecastilloasport@gmail.com>
2022-05-30 18:18:08 -0500

    [Add]Add style for better view in mobile screen size. Improve responsiveness

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -8,10 +8,6 @@ function Block(props){
             <span className={props.capLetterColor}>B</span>
             {props.paragraph1}
           </p>
-          <p className="line-break margin-top-10"></p>
-          <p className="margin-top-10">
-            {props.paragraph2}
-          </p>
         </div>
       </section>
     );
