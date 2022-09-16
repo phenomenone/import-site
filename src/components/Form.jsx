d837d77e5cf09cc177c6db7eabb48f3e66499ede
phenomenone <ecastilloasport@gmail.com>
2022-09-15 19:15:23 -0500

    [Add] Add logic for mailchimp form.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,42 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Form() {
+
+  const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fMessage, setFMessage] = useState('');
+
+
+
+  console.log(process.env.REACT_APP_MAILCHIMP_URL);
+  console.log(process.env.REACT_APP_MAILCHIMP_U);
+  console.log(process.env.REACT_APP_MAILCHIMP_ID);
+  
   return (
     <div className="container">
-      <form className="contact-form" action="action_page.php">
-        {/* <label for="fname">First Name</label> */}
-        <input
-          type="text"
-          id="fname"
-          name="firstname"
-          placeholder="Nombre(s) ..."
-        />
+      <form className="contact-form" action={process.env.REACT_APP_MAILCHIMP_URL} method="POST" noValidate>
+        <input type="hidden" name="u" value={process.env.REACT_APP_MAILCHIMP_U}/>
+        <input type="hidden" name="id" value={process.env.REACT_APP_MAILCHIMP_ID}/>
+          <label htmlFor='MERGE0'>
+                    {/* Email */}
+                    <input 
+                        type="email" 
+                        name="EMAIL" 
+                        id="MERGE0"
+                        value={email} 
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                        autoCapitalize="off" 
+                        autoCorrect="off"
+                        placeholder="Correo electrónico"
+                     /> 
+                </label>
+                <label htmlFor='MERGE1'>
+                    {/* First name */}
+                    <input 
+                        type="text" 
+                        name="FNAME" 
+                        id="MERGE1" 
+                        value={firstName} 
+                        onChange={(e)=>{setFirstName(e.target.value)}}
+                        placeholder="Nombre(s) ..."
+                    />
+                </label>
+                <label htmlFor='MERGE2'>
+                    {/* Last name */}
+                    <input 
+                        type="text" 
+                        name="LNAME" 
+                        id="MERGE2" 
+                        value={lastName} 
+                        onChange={(e)=>{setLastName(e.target.value)}}
+                        placeholder="Apellido(s) ..."
+                    />
+                </label>
+                <label htmlFor='MERGE3'>
+                    {/* Message */}
+                    <textarea 
+                        type="text" 
+                        name="MESSAGE" 
+                        id="MERGE3" 
+                        value={fMessage} 
+                        onChange={(e)=>{setFMessage(e.target.value)}}
+                        style={{ height: "200px" }}
+                        placeholder="Escriba su mensaje aca. Recuerde enviar su informacion de contacto ..."
+                    ></textarea>
+                </label>
 
-        {/* <label for="lname">Last Name</label> */}
-        <input
-          type="text"
-          id="lname"
-          name="lastname"
-          placeholder="Apellido(s) ..."
-        />
-
-        {/* <label for="lname">Last Name</label> */}
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Correo @ Electroncico ..."
-        />
-
-        {/* <label for="subject">Subject</label> */}
-        <textarea
-          id="subject"
-          name="subject"
-          placeholder="Escriba su mensaje aca. Recuerde enviar su informacion de contacto ..."
-          style={{ height: "200px" }}
-        ></textarea>
-
-        <input type="submit" value="Enviar" />
+        <input type="submit" value="Enviar"/>
       </form>
     </div>
   );
