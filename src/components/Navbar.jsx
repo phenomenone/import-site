24bdb006bfbe680d18fac3bfa557d780a3c1179e
phenomenone <ecastilloasport@gmail.com>
2022-09-21 17:46:08 -0500

    [Add] Improve navbar style, responsiveness and design.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,16 @@ import React from "react";
 function Navbar (){
 
     return (
-      <div>
-    <header class="page-header">
-        <nav class="page-header-navbar">
-        <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Refrences</li>
-          <li>Investors</li>
-        </ul>
-      </nav>
-    </header>
-    {/* <main class="page-container">
-      <h1 class="hero-section-title">Future is already here...</h1>
-    </main> */}
+    <div>
+      <header class="page-header">
+          <nav class="page-header-navbar">
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre Nosotros</li>
+            <li>Contactanos</li>
+          </ul>
+        </nav>
+      </header>
     </div>
     );
 
