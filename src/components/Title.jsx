1c25d4a30e47509074958476e48b8cd2596440bb
phenomenone <ecastilloasport@gmail.com>
2022-09-26 12:20:47 -0500

    [Up]Updated navbar aand paralax boxes design. Revert/remove animation

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,9 +4,8 @@ function Title(){
     return (
         <section>
             <div className="title">
-            <p className='shinning'>Desde el</p>
-            {/* <h3 className='shinning'>Desde el</h3> */}
-            <h1 className='shinning'>MUNDO</h1>
+            <h3>Desde el</h3>
+            <h1>MUNDO</h1>
             </div>
         </section>
         );
