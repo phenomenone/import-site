cafb7330c69057f2d14c39970ff7dcadc5e6fd92
phenomenone <ecastilloasport@gmail.com>
2022-09-28 16:40:55 -0500

    [Add] Add new icons library react-icons. Remove ffontawesome icons packages. Imporve footer design and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,37 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTwitter,
-  faFacebook,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { AiFillFacebook, AiFillTwitterCircle, AiFillInstagram, AiFillMail  } from 'react-icons/ai'
+import { IconContext } from "react-icons";
+
 
 function Footer() {
   return (
     <div className="social-container" id="scroll-footer">
       <div className="social-item">
         <a href="https://twitter.com/Desdelmundo" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faTwitter} />
+        <IconContext.Provider value={{ style: { fontSize: "2.5em"  } }}>
+          <AiFillTwitterCircle />
+          </IconContext.Provider>
         </a>
       </div>
       <div className="social-item">
         <a href="https://www.facebook.com/profile.php?id=100086414527208" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faFacebook} />
+        <IconContext.Provider value={{ style: { fontSize: "2.5em"  } }}>
+          <AiFillFacebook />
+          </IconContext.Provider>
         </a>
       </div>
       <div className="social-item">
         <a href="https://www.instagram.com/dsdlmundo/" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faInstagram} />
+        <IconContext.Provider value={{ style: { fontSize: "2.5em"  } }}>
+          <AiFillInstagram />
+          </IconContext.Provider>
         </a>
       </div>
       <div className="social-item">
         <a href="mailto:ventas@desdeelmundo.com">
-          <FontAwesomeIcon icon={faEnvelope} />
+        <IconContext.Provider value={{ style: { fontSize: "2.5em"  } }}>
+          <AiFillMail />
+          </IconContext.Provider>
         </a>
       </div>
     </div>
